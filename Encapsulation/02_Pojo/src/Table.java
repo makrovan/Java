02a136d60fee61e29aafff7987a29411f3599d93
Anton Makrov <makrov_an@mail.ru>
2022-02-13 14:53:28 +0800

    Добавлен мой класс Table.

---
+++ Encapsulation/02_Pojo/src/Table.java
@@ -0,0 +1,53 @@
+public class Table {
+    private int length;
+    private int width;
+    private int height;
+    private String color;
+    private String material;
+
+    public Table(int length, int width, String color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
