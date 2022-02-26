70b03fc4d010c6e01b131737aeb9fa9bf34c699e
Anton Makrov <makrov_an@mail.ru>
2022-02-26 22:06:06 +0800

    Исправлена ошибка в классе Container.

--- PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,9 +1,7 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0;
 
-    public void addCount(int value) {
-        count = count + value;
-    }
+    public void addCount(int value) { count = count + value;}
 
     public int getCount() {
         return count;
