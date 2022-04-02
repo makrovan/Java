05cb13352072588fad66aff27cc96da5bc75eb1d
Anton Makrov <makrov_an@mail.ru>
2022-04-02 15:34:45 +0800

    Выполнено задание 1

--- AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.util.Calendar;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,8 +15,28 @@ public class Main {
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
+        // Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
+        /*Stream<Employee> stream = staff.stream();
+        //stream.forEach(System.out::println);
+        stream = staff.stream().filter(employee -> {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(employee.getWorkStart());
+            return cal.get(Calendar.YEAR) == year;
+        });
+        //stream.forEach(System.out::println);
+        Optional<Employee> optional = stream.max(Comparator.comparing(Employee::getSalary));
+        *//*Optional<Employee> optional = stream.max(new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getSalary().compareTo(o2.getSalary());
+            }
+        });*//*
+        //optional.ifPresent(System.out::println);
+        //stream.max(Comparator.comparing(Employee::getSalary));
+        Employee employee = optional.get();
+        return employee;*/
+        return staff.stream().filter(e -> e.getWorkStart().getYear() == year - 1900).
+                max(Comparator.comparing(Employee::getSalary)).get();
     }
 }
