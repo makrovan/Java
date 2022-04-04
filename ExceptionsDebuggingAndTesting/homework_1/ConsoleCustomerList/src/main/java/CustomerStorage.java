1ed2372a19293381e7cfabaca196741e8b71adab
Anton Makrov <makrov_an@mail.ru>
2022-04-04 12:21:36 +0800

    дополнительно найдены исключения при вызове команд add и remove без параметров.

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -13,8 +13,18 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
+        final int NUMBER_OF_DATA_ELEMENTS = 4;
 
         String[] components = data.split("\\s+");
+        if (components.length != NUMBER_OF_DATA_ELEMENTS) {
+            throw new IllegalArgumentException("Wrong format in add command!");
+        }
+        if (!components[INDEX_PHONE].matches("[+7][0-9]{11}")) {
+            throw new IllegalArgumentException("Wrong format in phone number!");
+        }
+        if (!components[INDEX_EMAIL].matches("[a-zA-Z.]+[@][a-zA-Z]+[.][a-zA-Z]{2,}")) {
+            throw new IllegalArgumentException("Wrong format in e-mail!");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
