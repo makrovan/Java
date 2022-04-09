2bed303071f2dba2f488fcc8dfd49ddaf0ff1649
Anton Makrov <makrov_an@mail.ru>
2022-04-09 14:11:42 +0800

    Homework 15.7 done!

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+    private static File setDestFileFullName(File srcFile, File destFile) {
+        return new File(destFile.getAbsolutePath() + File.separator + srcFile.getName());
+    }
+
+    private static File mkDir(File srcFile, File destFile) {
+        destFile = setDestFileFullName(srcFile, destFile);
+        destFile.mkdir();
+        return destFile;
+    }
+
+    private static void copyFile (File srcFile, File destFile) throws IOException {
+        Files.copy(srcFile.toPath(), destFile.toPath());
+    }
+
+
+    private static void copyFileFolder(File srcFile, File destFile) throws IOException {
+        File[] filesToCopy = srcFile.listFiles();
+        assert filesToCopy != null;
+        for (File file : filesToCopy) {
+            if (file.isDirectory()) {
+                File copyToDir = mkDir(file, destFile);
+                copyFileFolder(file, copyToDir);
+            } else {
+                File copyToFile = new File(destFile.getAbsolutePath() + File.separator + file.getName());
+                copyFile(file, copyToFile);
+            }
+        }
+    }
+
+    public static void copyFolder (String sourceFolder, String destFolder) throws IOException {
+        File srcFile = new File(sourceFolder);
+        File destFile = new File(destFolder);
+        copyFileFolder(srcFile, destFile);
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите путь до копируемой директории: ");
+            String sourceDirectory = in.nextLine();
+            if (!new File(sourceDirectory).isDirectory()) {
+                throw new IllegalArgumentException("Введен неверный путь директории!");
+            }
+
+            System.out.print("Введите путь куда скопировать: ");
+            String destinationDirectory = in.nextLine();
+            if (!new File(sourceDirectory).isDirectory()) {
+                throw new IllegalArgumentException("Введен неверный путь директории!");
+            }
+
+            FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+            System.out.println("Копирование завершено.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
