cabded965dde0a233cba1d36fe958df8910a69a6
Anton Makrov <makrov_an@mail.ru>
2022-04-12 00:31:53 +0800

    Homework_15.9 done!

--- FilesAndNetwork/homework_3/src/main/java/Main.java
+++ FilesAndNetwork/homework_3/src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.text.DecimalFormat;
+
 public class Main {
+    private static final String csvFile = "/Users/anton/IdeaProjects/java_basics/FilesAndNetwork/" +
+            "homework_3/src/test/resources/movementList.csv";
+
     public static void main(String[] args) {
+
+        Movements movements = new Movements(csvFile);
+        DecimalFormat df = new DecimalFormat("###,###.##");
+        System.out.println("Сумма расходов: " + df.format(movements.getExpenseSum()) + " руб.");
+        System.out.println("Сумма доходов: " + df.format(movements.getIncomeSum()) + " руб.");
+        System.out.println("Суммы расходов по организациям:");
+        movements.getExpenseSumByOrganisation().forEach(System.out::println);
     }
 }
