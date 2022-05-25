d3e2c956a5f4ec697b285c80e29c5af3d4d566f7
Anton Makrov <makrov_an@mail.ru>
2022-05-26 00:25:03 +0800

    Homework_19.14 done!

---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/model/Deal.java
@@ -0,0 +1,22 @@
+package main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Deal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String toDo;
+}
